d1cd77a461eccdc7d5f56336d8c96e6dcb53b37c
dafengge0913 <dafengge0913@hotmail.com>
2017-01-19 15:53:07 +0800

    CPU

--- monitor.sh
+++ monitor.sh
@@ -18,5 +18,20 @@ function showMem()
 	echo "Mem Cache: $cache"
 }
 
-showMem
+# cpu usage
+function showCpu()
+{
+	# vmstat the first output is inaccurate
+	eval $(echo `vmstat 1 3 | awk '{if(NR==5) print "total="100-$15"%;user="$13"%;system="$14"%;iowait="$16"%"}'`)
+	echo "Cpu Total:" $total
+	echo "Cpu User:" $user
+	echo "Cpu System:" $system
+	echo "Cpu IOWait:" $iowait
+
+	eval $(echo `uptime | awk -F " load average: " '{print $2}'|awk -F ", " '{print "la1="$1";la5="$2";la15="$3}'`)
+	echo "Load average 1 min:"$la1", 5 min:"$la5", 15 min:"$la15
+}
+
+#showMem
+showCpu
 
